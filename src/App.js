75f60b580bb024dd0510978a53848444e2a764a4
vishnu2193 <81963203+vishnu2193@users.noreply.github.com>
2021-11-24 22:35:38 +0530

    yes yes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      inputs: []
+      inputs: [],
+      posts: [],
+      search: ""
     };
   }
-
+  componentDidMount() {
+    axios({
+      url: `https://jsonplaceholder.typicode.com/posts`,
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+      .then((responce) => {
+        this.setState({ posts: responce.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   addInput = (ev) => {
     this.setState((prev) => ({ inputs: [...prev.inputs, "HI"] }));
   };
-
+  handle = (e) => {
+    this.setState({ search: e.target.value });
+  };
   render() {
+    const { posts, search } = this.state;
     return (
       <div>
+        {console.log(posts)}
         <button onClick={this.addInput}>Add input</button>
         <br />
         {this.state.inputs.map((i) => {
@@ -28,6 +47,21 @@ class App extends React.Component {
             </>
           );
         })}
+        <input onChange={this.handle} value={search} />
+        <div>
+          {posts
+            .filter((item) =>
+              item.title.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((item) => {
+              return (
+                <div>
+                  <p>{item.title}</p>
+                  <p>{item.id}</p>
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
