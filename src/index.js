8b5a0f4a37079f29131b3161d15522986d86e11e
vishnu2193 <81963203+vishnu2193@users.noreply.github.com>
2021-12-11 11:58:47 +0530

    yes yes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Button_colorchange from "./Button_colorchange";
 import Imagedelete from "./Imagedelete";
+import Header from "./Header";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App name="vishnu" />
+    <Header />
   </StrictMode>,
   rootElement
 );
